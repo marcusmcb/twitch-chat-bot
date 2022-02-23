1389f3b0647bd8ef1170fae97151cea6248099d9
Marcus McBride <marcusmcb@gmail.com>
2022-02-23 14:45:14 -0800

    added 8ball command, moved responses to ext module

---
+++ data/8ball.js
@@ -0,0 +1,48 @@
+//  Positive
+let positiveOutcome1 = 'It is certain.'
+let positiveOutcome2 = 'It is decidedly so.'
+let positiveOutcome3 = 'Without a doubt.'
+let positiveOutcome4 = 'Definitely.'
+let positiveOutcome5 = 'You may rely on it.'
+let positiveOutcome6 = 'As I see it, yes.'
+let positiveOutcome7 = 'Most likely.'
+let positiveOutcome8 = 'Outlook good.'
+let positiveOutcome9 = 'Yes.'
+let positiveOutcome10 = 'Signs point to yes.'
+//  Non-Committal
+let neutralOutcome1 = 'Reply hazy, try again.'
+let neutralOutcome2 = 'Not now, kid... kinda busy.'
+let neutralOutcome3 = 'Better not tell you now.'
+let neutralOutcome4 = 'Cannot predict now.'
+let neutralOutcome5 = 'Concentrate and ask again.'
+// Negative
+let negativeOutcome1 = "Don't count on it."
+let negativeOutcome2 = 'You would think so... but no.'
+let negativeOutcome3 = 'No idea.  Try Googling that.'
+let negativeOutcome4 = 'Outlook not so good.'
+let negativeOutcome5 = 'Very doubtful.'
+
+let eightBallMessages = [
+  positiveOutcome1,
+  positiveOutcome2,
+  positiveOutcome3,
+  positiveOutcome4,
+  positiveOutcome5,
+  positiveOutcome6,
+  positiveOutcome7,
+  positiveOutcome8,
+  positiveOutcome9,
+  positiveOutcome10,
+  neutralOutcome1,
+  neutralOutcome2,
+  neutralOutcome3,
+  neutralOutcome4,
+  neutralOutcome5,
+  negativeOutcome1,
+  negativeOutcome2,
+  negativeOutcome3,
+  negativeOutcome4,
+  negativeOutcome5,
+]
+
+module.exports = eightBallMessages
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const tmi = require('tmi.js');
+const tmi = require('tmi.js')
 const dotenv = require('dotenv')
+
+// import 8ball response array
+const eightBallMessages = require('./data/8ball')
 
 dotenv.config()
 
@@ -7,22 +10,44 @@ const client = new tmi.Client({
   options: { debug: true },
   connection: {
     secure: true,
-    reconnect: true
+    reconnect: true,
   },
   identity: {
-    username: 'djmarcusmcb',
-    password: process.env.TWITCH_OAUTH_TOKEN
+    username: process.env.TWITCH_BOT_USERNAME,
+    password: process.env.TWITCH_OAUTH_TOKEN,
   },
-  channels: ['djmarcusmcb']
-});
+  channels: ['djmarcusmcb'],
+})
 
-client.connect();
+client.connect()
 
 client.on('message', (channel, tags, message, self) => {
-  // Ignore echoed messages.
-  if(self) return;
-
-  if(message.toLowerCase() === '!hello') {
-    client.say(channel, `@${tags.username}, Yo what's up`);
+  if (self || !message.startsWith('!')) {
+    return
   }
-});
+
+  const args = message.slice(1).split(' ')
+  const command = args.shift().toLowerCase()
+
+  console.log('ARGS: ', args)
+  console.log('COMMAND: ', command)
+
+  if (command === 'echo') {
+    client.say(channel, `@${tags.username}, you said: "${args.join(' ')}"`)
+  } else if (command === 'hello') {
+    client.say(channel, `@${tags.username}, Yo what's up`)
+  } else if (command === 'dice') {
+    const result = Math.floor(Math.random() * 6) + 1
+    client.say(channel, `@${tags.username}, You rolled a ${result}.`)
+  } else if (command === '8ball') {
+    // check if user entered anything after the command
+    if (args.length != 0) {
+      // generate random index
+      var random_key = Math.floor(Math.random() * 19 - 0) + 0
+      client.say(channel, `@${tags.username}, ${eightBallMessages[random_key]}`)
+    } else {
+      // if not, generic response to prompt the user to try again
+      client.say(channel, `@${tags.username}, what'cha wanna know?`)
+    }
+  }
+})
