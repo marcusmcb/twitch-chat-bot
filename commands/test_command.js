906f0f3b2872ff12ec79014f90cc03b68eb16802
Marcus McBride <marcusmcb@gmail.com>
2023-06-21 12:04:37 -0700

    removed axios cxall from test command

--- commands/test_command.js
+++ commands/test_command.js
@@ -1,20 +1,20 @@
-const axios = require('axios')
+// const axios = require('axios')
 
 const testCommand = (channel, tags, args, client) => {
   client.say(
     channel,
     'Your Twitch chat is properly linked to this script!'
   )
-  let message = "Hello from twitch.tv/djmarcusmcb"
-  axios.post('http://192.168.86.50:8000/display', {
-    message: message
-  })
-    .then((response) => {
-      console.log("Response from Pi: ", response.data)
-    })
-    .catch((error) => {
-      console.log("ERROR: ", error)
-    })
+  // let message = "Hello from twitch.tv/djmarcusmcb"
+  // axios.post('http://192.168.86.50:8000/display', {
+  //   message: message
+  // })
+  //   .then((response) => {
+  //     console.log("Response from Pi: ", response.data)
+  //   })
+  //   .catch((error) => {
+  //     console.log("ERROR: ", error)
+  //   })
 }
 
 module.exports = {
