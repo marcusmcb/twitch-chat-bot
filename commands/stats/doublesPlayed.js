fd6487c36dc3f8d716c12e43bbfb69da4ef8e760
Marcus McBride <marcusmcb@gmail.com>
2023-07-02 17:09:58 -0700

    refactored doubles command into separate module

---
+++ commands/stats/doublesPlayed.js
@@ -0,0 +1,51 @@
+const dotenv = require('dotenv')
+
+const createLiveReport = require('./createLiveReport')
+
+dotenv.config()
+
+// const url = `https://serato.com/playlists/${process.env.SERATO_DISPLAY_NAME}/live`;
+const url = 'https://serato.com/playlists/DJ_Marcus_McBride/3-12-2022'
+
+const doublesCommand = async (channel, tags, args, client, obs) => {
+	try {
+		const reportData = await createLiveReport(url)
+		if (reportData.total_tracks_played === 0) {
+			client.say(
+				channel,
+				'Sorry, no playlist stats for this stream at the moment.'
+			)
+		} else {
+			client.say(
+				channel,
+				`${channel.slice(1)} has rocked doubles ${
+					reportData.doubles_played.length
+				} times so far in this set.`
+			)
+		}
+		obs.call('SetInputSettings', {
+			inputName: 'obs-chat-response',
+			inputSettings: {
+				text: `${tags.username} has rocked doubles\n${
+					reportData.doubles_played.length
+				} times so far in this set.\n\nLast song he played doubles with:\n${
+					reportData.doubles_played[reportData.doubles_played.length - 1].name
+				}`,
+			},
+		})
+		setTimeout(() => {
+			obs.call('SetInputSettings', {
+				inputName: 'obs-chat-response',
+				inputSettings: {
+					text: '',
+				},
+			})
+		}, 10000)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+module.exports = {
+  doublesCommand: doublesCommand
+}
--- commands/stats/stats.js
+++ commands/stats/stats.js
@@ -1,14 +1,4 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
 const dotenv = require('dotenv')
-
-const {
-	parseDateAndTime,
-	convertMilliseconds,
-	parseTrackTimestamps,
-	calculateTimeDifferences,
-	calculateAverageTrackLength,
-} = require('./helpers/statsHelpers')
 
 const createLiveReport = require('./createLiveReport')
 
@@ -19,80 +9,25 @@ const url = 'https://serato.com/playlists/DJ_Marcus_McBride/3-12-2022'
 
 const statsCommand = async (channel, tags, args, client, obs) => {
 	try {
-		const { data } = await axios.get(url)
-		const $ = cheerio.load(data)
-
-		const playlistDate = $('span.playlist-start-time').first().text().trim()
-		const { timestampsParsed, trackTimestamps } = parseTrackTimestamps(
-			$,
-			playlistDate
-		)
-		const timeDiffs = calculateTimeDifferences(trackTimestamps)
-		const averageTrackLength = calculateAverageTrackLength(timeDiffs)
-		let reportData
-		await createLiveReport(url).then((data) => {
-			console.log('DATA: ')
-			console.log(data)
-			reportData = data
-		})
-
-		reportData.doubles_played.forEach((item) => console.log(item))
-
-		if (timeDiffs.length === 0) {
+		const reportData = await createLiveReport(url)
+		console.log(reportData.track_array)
+		if (reportData.total_tracks_played === 0) {
 			client.say(
 				channel,
 				'Sorry, no playlist stats for this stream at the moment.'
 			)
 		} else {
-			// client.say(
-			// 	channel,
-			// 	`${channel.slice(1)} has played ${
-			// 		timeDiffs.length + 1
-			// 	} songs so far in this set with an average length of ${averageTrackLength} per song.`
-			// )
-
-			// client.say(
-			// 	channel,
-			// 	`${channel.slice(1)} has rocked doubles ${
-			// 		reportData.doubles_played.length
-			// 	} times so far in this set.`
-			// )
-
 			client.say(
 				channel,
-				`The longest song in ${channel.slice(1)}'s set (so far) is: ${
-					reportData.longest_track.name
-				} (${reportData.longest_track.length_value})`
+				`${tags.username} has played ${reportData.total_tracks_played} songs so far in this set with an average length of ${reportData.average_track_length} per song.`
 			)
 
 			obs.call('SetInputSettings', {
 				inputName: 'obs-chat-response',
 				inputSettings: {
-					text: `${tags.username} has played ${
-						timeDiffs.length + 1
-					} songs so far\nin this stream at an average of ${averageTrackLength} per song`,
+					text: `${tags.username} has played ${reportData.total_tracks_played} songs so far\nin this stream at an average of ${reportData.average_track_length} per song.`,
 				},
 			})
-
-			obs.call('SetInputSettings', {
-				inputName: 'obs-chat-response',
-				inputSettings: {
-					text: `${channel.slice(1)} has rocked doubles\n${
-						reportData.doubles_played.length
-					} times so far in this set.\n\nLast song he played doubles with:\n${
-						reportData.doubles_played[reportData.doubles_played.length - 1].name
-					}`,
-				},
-			})
-
-			// obs.call('SetInputSettings', {
-			// 	inputName: 'obs-chat-response',
-			// 	inputSettings: {
-			// 		text: `Longest song in ${channel.slice(1)}'s set so far?\n\n${
-			// 			reportData.longest_track.name
-			// 		}\n(${reportData.longest_track.length_value})`,
-			// 	},
-			// })
 
 			setTimeout(() => {
 				obs.call('SetInputSettings', {
@@ -109,6 +44,122 @@ const statsCommand = async (channel, tags, args, client, obs) => {
 	}
 }
 
+const longestTrackCommand = async (channel, tags, args, client, obs) => {
+	try {
+		const reportData = await createLiveReport(url)
+		if (reportData.total_tracks_played === 0) {
+			client.say(
+				channel,
+				'Sorry, no playlist stats for this stream at the moment.'
+			)
+		} else {
+			client.say(
+				channel,
+				`The longest song in ${tags.username}'s set (so far) is ${reportData.longest_track.name} (${reportData.longest_track.length_value})`
+			)
+		}
+		obs.call('SetInputSettings', {
+			inputName: 'obs-chat-response',
+			inputSettings: {
+				text: `Longest song in ${tags.username}'s set so far: \n\n${reportData.longest_track.name}\n(${reportData.longest_track.length_value})`,
+			},
+		})
+		setTimeout(() => {
+			obs.call('SetInputSettings', {
+				inputName: 'obs-chat-response',
+				inputSettings: {
+					text: '',
+				},
+			})
+		}, 10000)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+const shortestTrackCommand = async (channel, tags, args, client, obs) => {
+	try {
+		const reportData = await createLiveReport(url)
+		if (reportData.total_tracks_played === 0) {
+			client.say(
+				channel,
+				'Sorry, no playlist stats for this stream at the moment.'
+			)
+		} else {
+			client.say(
+				channel,
+				`The shortest song in ${tags.username}'s set (so far) is ${reportData.shortest_track.name} (${reportData.shortest_track.length_value})`
+			)
+		}
+		obs.call('SetInputSettings', {
+			inputName: 'obs-chat-response',
+			inputSettings: {
+				text: `Shortest song in ${tags.username}'s set so far: \n\n${reportData.shortest_track.name}\n(${reportData.shortest_track.length_value})`,
+			},
+		})
+		setTimeout(() => {
+			obs.call('SetInputSettings', {
+				inputName: 'obs-chat-response',
+				inputSettings: {
+					text: '',
+				},
+			})
+		}, 10000)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+
+
+const dypCommand = async (channel, tags, args, client, obs) => {
+	if (args.length === 0) {
+		client.say(
+			channel,
+			`Add an artist's name after the command to see if ${tags.username} has played them yet in this stream.`
+		)
+	} else {
+		try {
+			const reportData = await createLiveReport(url)
+			let searchResults = []
+			let searchTerm = `${args}`.replaceAll(',', ' ')
+			for (let i = 0; i < reportData.track_array.length; i++) {
+				// console.log(reportData.track_array[i])
+				if (
+					reportData.track_array[i]
+						.toLowerCase()
+						.includes(searchTerm.toLowerCase())
+				) {
+					searchResults.push(reportData.track_array[i])
+				}
+			}
+
+			if (searchResults.length === 0) {
+				client.say(
+					channel,
+					`${tags.username} has not played ${searchTerm} so far in this stream.`
+				)
+			} else if (searchResults.length === 1) {
+				client.say(
+					channel,
+					`${tags.username} has played ${searchTerm} ${searchResults.length} time so far in this stream.`
+				)
+			} else if (searchResults.length > 1) {
+				console.log(searchResults)
+				client.say(
+					channel,
+					`${tags.username} has played ${searchTerm} ${searchResults.length} times so far in this stream.`
+				)
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+}
+
 module.exports = {
-	statsCommand: statsCommand,
+	statsCommand: statsCommand,	
+	longestTrackCommand: longestTrackCommand,
+	shortestTrackCommand: shortestTrackCommand,
+	dypCommand: dypCommand,
 }
