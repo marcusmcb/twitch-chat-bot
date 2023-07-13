fca55374d7b7185717d8ae0e30361a25db7f4af1
Marcus McBride <marcusmcb@gmail.com>
2023-07-12 18:53:10 -0700

    refactored stats command for cleaner code

--- commands/stats/stats.js
+++ commands/stats/stats.js
@@ -1,13 +1,34 @@
 const createLiveReport = require('./createLiveReport')
-
 const dotenv = require('dotenv')
 
 dotenv.config()
 
+const displayStatsMessage = (obs, tags, reportData, direction) => {
+	const message = `${tags.username} has played ${
+		reportData.total_tracks_played
+	} songs so far\nin this stream at an average of ${
+		reportData.average_track_length
+	} per song ${direction}${parseInt(reportData.average_change.difference)}%)`
+	obs.call('SetInputSettings', {
+		inputName: 'obs-chat-response',
+		inputSettings: {
+			text: message,
+		},
+	})
+	setTimeout(() => {
+		obs.call('SetInputSettings', {
+			inputName: 'obs-chat-response',
+			inputSettings: {
+				text: '',
+			},
+		})
+	}, 5000)
+}
+
 const statsCommand = async (channel, tags, args, client, obs, url) => {
 	try {
 		const reportData = await createLiveReport(url)
-		const averageTrackLength = reportData.average_track_length
+		console.log(typeof reportData.average_change.difference)
 		if (reportData.total_tracks_played === 0) {
 			client.say(
 				channel,
@@ -16,47 +37,18 @@ const statsCommand = async (channel, tags, args, client, obs, url) => {
 		} else if (reportData.average_change.isLarger) {
 			client.say(
 				channel,
-				`${tags.username} has played ${reportData.total_tracks_played} songs so far in this set with an average length of ${averageTrackLength} per song.`
+				`${tags.username} has played ${reportData.total_tracks_played} songs so far in this set with an average length of ${reportData.average_track_length} per song.`
 			)
-			obs.call('SetInputSettings', {
-				inputName: 'obs-chat-response',
-				inputSettings: {
-					text: `${tags.username} has played ${reportData.total_tracks_played} songs so far\nin this stream at an average of ${averageTrackLength} per song (↑${reportData.average_change.difference}%)`,
-				},
-			})
-
-			setTimeout(() => {
-				obs.call('SetInputSettings', {
-					inputName: 'obs-chat-response',
-					inputSettings: {
-						text: '',
-					},
-				})
-			}, 5000)
+			displayStatsMessage(obs, tags, reportData, '(↑')
 		} else if (
 			!reportData.average_change.isLarger &&
 			reportData.average_change.difference !== null
 		) {
 			client.say(
 				channel,
-				`${tags.username} has played ${reportData.total_tracks_played} songs so far in this set with an average length of ${averageTrackLength} per song.`
+				`${tags.username} has played ${reportData.total_tracks_played} songs so far in this set with an average length of ${reportData.average_track_length} per song.`
 			)
-			obs.call('SetInputSettings', {
-				inputName: 'obs-chat-response',
-				inputSettings: {
-					text: `${tags.username} has played ${reportData.total_tracks_played} songs so far\nin this stream at an average of ${averageTrackLength} per song (↓${reportData.average_change.difference}%)`,
-				},
-			})
-
-			setTimeout(() => {
-				obs.call('SetInputSettings', {
-					inputName: 'obs-chat-response',
-					inputSettings: {
-						text: '',
-					},
-				})
-			}, 5000)
-		} else {
+			displayStatsMessage(obs, tags, reportData, '(↓')
 		}
 	} catch (err) {
 		console.log(err)
