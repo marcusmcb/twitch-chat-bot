89edfc4b53e508ad8d07993c70710e679095883f
marcusmcb <marcusmcb@gmail.com>
2023-09-05 11:51:06 -0700

    added logic to display lotto response in chat only if obs disabled

--- commands/lotto/lotto.js
+++ commands/lotto/lotto.js
@@ -1,15 +1,8 @@
 const axios = require('axios')
+const dotenv = require('dotenv')
+dotenv.config()
 
-const clearOBSResponse = (obs, obsClearDisplayTime) => {
-	setTimeout(() => {
-		obs.call('SetInputSettings', {
-			inputName: 'obs-chat-response',
-			inputSettings: {
-				text: '',
-			},
-		})
-	}, parseInt(obsClearDisplayTime * 1000, 10))
-}
+const obsEnabled = process.env.DISPLAY_OBS_MESSAGES
 
 const lottoCommand = async (channel, tags, args, client, obs) => {
 	const numberSet = new Set()
@@ -19,47 +12,47 @@ const lottoCommand = async (channel, tags, args, client, obs) => {
 	}
 	const lotteryNumbers = Array.from(numberSet)
 		.sort((a, b) => a - b)
-		.join(', ')
+		.join(', ')	
 
-	client.say(
-		channel,
-		`${tags.username}, check the PiBot for your lucky numbers!`
-	)
+	if (obsEnabled === 'true') {		
+		client.say(
+			channel,
+			`${tags.username}, check the PiBot for your lucky numbers!`
+		)
 
-	let currentScene
-	let message = `${tags.username}'s lucky numbers: ${lotteryNumbers}`
+		let currentScene
+		let message = `${tags.username}'s lucky numbers: ${lotteryNumbers}`
 
-	await obs.call('GetSceneList').then((data) => {
-		console.log("CURRENT SCENE: ", data.currentProgramSceneName)
-		currentScene = data.currentProgramSceneName
-	})
+		await obs.call('GetSceneList').then((data) => {
+			console.log('CURRENT SCENE: ', data.currentProgramSceneName)
+			currentScene = data.currentProgramSceneName
+		})	
 
-	// obs.call('SetInputSettings', {
-	// 	inputName: 'obs-chat-response',
-	// 	inputSettings: {
-	// 		text: `${tags.username}, check the PiBot for your lucky numbers!`,
-	// 	},
-	// })
-	// clearOBSResponse(obs, 5)
-	axios
-		.post('http://192.168.86.50:8000/display', {
-			message: message,
-		})
-		.then((response) => {
-			console.log('Response from Pi: ', response.data)
-		})
-		.catch((error) => {
-			console.log('ERROR: ', error)
-		})
-	setTimeout(async () => {
-		await obs
-			.call('SetCurrentProgramScene', { sceneName: 'LIVE VIEW - Main' })
-			.then((data) => console.log(data))
-	}, 2000)
+		axios
+			.post('http://192.168.86.50:8000/display', {
+				message: message,
+			})
+			.then((response) => {
+				console.log('Response from Pi: ', response.data)
+			})
+			.catch((error) => {
+				console.log('ERROR: ', error)
+			})
+		setTimeout(async () => {
+			await obs
+				.call('SetCurrentProgramScene', { sceneName: 'LIVE VIEW - Main' })
+				.then((data) => console.log(data))
+		}, 2000)
 
-	setTimeout(() => {
-		obs.call('SetCurrentProgramScene', { sceneName: `${currentScene}` })
-	}, 20000)
+		setTimeout(() => {
+			obs.call('SetCurrentProgramScene', { sceneName: `${currentScene}` })
+		}, 10000)
+	} else {
+		client.say(
+			channel,
+			`${tags.username}'s lucky numbers are: ${lotteryNumbers}`
+		)
+	}
 }
 
 module.exports = {
