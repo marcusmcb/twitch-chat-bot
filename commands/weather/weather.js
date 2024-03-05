e7b24baaf1e5cc5d0e502aec5a408b12b2aa39a4
marcusmcb <marcusmcb@gmail.com>
2024-03-04 17:48:50 -0800

    fixed parsing error in temperature returned from weather command

--- commands/weather/weather.js
+++ commands/weather/weather.js
@@ -1,52 +1,61 @@
-const axios = require("axios");
+const axios = require('axios')
+
+// helper method to parse pre-decimal value
+// from temperature response returned
+
+const getTemperature = (temperature) => {  
+	const temperatureStr = temperature.toString()
+	const decimalIndex = temperatureStr.indexOf('.')
+	if (decimalIndex === -1) {
+		return temperatureStr
+	}
+	return temperatureStr.substring(0, decimalIndex)
+}
 
 const weatherCommand = async (channel, tags, args, client) => {
-  // check if user entered location
-  if (args.length != 0) {
-    let weather, userLocation, conditions, temperature, windSpeed;
-    // remove commas from parsed location
-    if (args.length > 1) {
-      userLocation = args.join(" ");
-    } else {
-      userLocation = args;
-    }
-    let weatherOptions = {
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${userLocation}&units=imperial&appid=${process.env.OPEN_WEATHER_API_KEY}`,
-      headers: { Accept: "application/json" },
-    };
-    try {
-      const response = await axios(weatherOptions);
-      if (response.status === 200) {
-        weather = response.data;        
-        conditions = weather.weather[0].main;
-        console.log("Conditions: ")
-        console.log(weather)
-        temperature = weather.main.temp.toString();
-        temperature = temperature.substring(0, temperature.length - 3);
-        windSpeed = weather.wind.speed.toFixed()
-        console.log(windSpeed)
-        client.say(
-          channel,
-          `Right now in ${userLocation}: ${conditions.toLowerCase()} with a temperature of ${temperature}F and ${
-            weather.main.humidity
-          }% humidity with winds at ${windSpeed} mph.`
-        );
-      } else {
-        client.say(channel, "Looks like that command isn't working right now.");
-      }
-    } catch (error) {
-      console.log(error);
-      client.say(channel, "Looks like that command isn't working right now.");
-    }
-  } else {
-    // ...prompt user to add their location
-    client.say(
-      channel,
-      `@${tags.username}, add your location after the command to get your weather!`
-    );
-  }
-};
+	// check if user entered location
+	if (args.length != 0) {
+		let weather, userLocation, conditions, temperature, windSpeed
+		// remove commas from parsed location
+		if (args.length > 1) {
+			userLocation = args.join(' ')
+		} else {
+			userLocation = args
+		}
+		let weatherOptions = {
+			url: `http://api.openweathermap.org/data/2.5/weather?q=${userLocation}&units=imperial&appid=${process.env.OPEN_WEATHER_API_KEY}`,
+			headers: { Accept: 'application/json' },
+		}
+		try {
+			const response = await axios(weatherOptions)
+			if (response.status === 200) {
+				weather = response.data
+				conditions = weather.weather[0].main
+				temperature = getTemperature(weather.main.temp)
+				windSpeed = weather.wind.speed.toFixed()
+
+				client.say(
+					channel,
+					`Right now in ${userLocation}: ${conditions.toLowerCase()} with a temperature of ${temperature}F and ${
+						weather.main.humidity
+					}% humidity with winds at ${windSpeed} mph.`
+				)
+			} else {
+				client.say(channel, "Looks like that command isn't working right now.")
+			}
+		} catch (error) {
+			console.log(error)
+			client.say(channel, "Looks like that command isn't working right now.")
+		}
+	} else {
+		// ...prompt user to add their location
+		client.say(
+			channel,
+			`@${tags.username}, add your location after the command to get your weather!`
+		)
+	}
+}
 
 module.exports = {
-  weatherCommand: weatherCommand,
-};
+	weatherCommand: weatherCommand,
+}
