4c9faf2e95c454a2879c9a0609dcee4336730697
marcusmcb <marcusmcb@gmail.com>
2024-05-25 18:26:49 -0700

    code cleanup on birb command

--- commands/birb/birb.js
+++ commands/birb/birb.js
@@ -8,38 +8,32 @@ const birbCommand = async (channel, tags, args, client, obs) => {
 		let currentScene
 		await obs.call('GetCurrentProgramScene').then((data) => {
 			currentScene = data.currentProgramSceneName
-		})
-
-		// Find the media source item in the current scene
+		})		
 		let sceneItemId
 		await obs
 			.call('GetSceneItemList', { sceneName: currentScene })
 			.then((data) => {
 				const sceneItem = data.sceneItems.find(
 					(item) => item.sourceName === 'Birdcam1'
-				) // Replace 'MediaSourceName' with the actual media source name
+				)
 				if (sceneItem) {
 					sceneItemId = sceneItem.sceneItemId
 				}
 			})
 
-		if (sceneItemId) {
-			// Set the enabled state of the media source to true
+		if (sceneItemId) {			
 			await obs.call('SetSceneItemEnabled', {
 				sceneName: currentScene,
 				sceneItemId: sceneItemId,
 				sceneItemEnabled: true,
 			})
-
-			setTimeout(async () => {
-				// Set the enabled state of the media source back to false
+			setTimeout(async () => {				
 				await obs.call('SetSceneItemEnabled', {
 					sceneName: currentScene,
 					sceneItemId: sceneItemId,
 					sceneItemEnabled: false,
 				})
-			}, 5000) // Adjust the duration as needed
-
+			}, 5000)
 			// client.say(channel, 'BIRB IS VISIBLE!')
 		} else {
 			client.say(channel, 'Media source not found in the current scene.')
