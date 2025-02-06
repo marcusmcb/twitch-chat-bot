75f651cc08355b8bd5fdfaea1600a9067126bf03
marcusmcb <marcusmcb@gmail.com>
2025-02-05 22:25:10 -0800

    finished countdown command

--- commands/countdown/countdown.js
+++ commands/countdown/countdown.js
@@ -1,5 +1,19 @@
 const countdownCommand = (channel, tags, args, client) => {
-  channel.say(client, "Countdown command is not implemented yet.")
+  if (args.length === 0) {
+    for (let i = 10; i > 0; i--) {
+      setTimeout(() => {
+        client.say(channel, `${i}...`)
+      }, (10 - i) * 1000)
+    }    
+  } else if (parseInt(args[0]) < 21) {
+    for (let i = parseInt(args[0]); i > 0; i--) {
+      setTimeout(() => {
+        client.say(channel, `${i}...`)
+      }, (parseInt(args[0]) - i) * 1000)
+    }
+  } else {
+    client.say(channel, `I get winded after 20 seconds. Try a lower number.`) 
+  } 
 }
 
 module.exports = { countdownCommand }
