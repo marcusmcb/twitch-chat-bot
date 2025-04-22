1f5d49a9965ac556a76598bb24281b5864f38e2b
marcusmcb <marcusmcb@gmail.com>
2025-04-22 11:06:23 -0700

    updated lotto command logic

--- commands/lotto/lotto.js
+++ commands/lotto/lotto.js
@@ -1,65 +1,87 @@
-const axios = require('axios')
-const dotenv = require('dotenv')
-dotenv.config()
+const WebSocket = require('ws')
 
-const obsEnabled = process.env.DISPLAY_OBS_MESSAGES
+const lottoCommand = async (
+	channel,
+	tags,
+	args,
+	client,
+	obs,
+	sceneChangeLock
+) => {
+	const obsEnabled = process.env.DISPLAY_OBS_MESSAGES
+	const wsAddress = 'ws://7.tcp.ngrok.io:21711' // Ngrok WebSocket address
 
-const lottoCommand = async (channel, tags, args, client, obs) => {	
-	const numberSet = new Set()
-	while (numberSet.size < 6) {
-		const randomNumber = Math.floor(Math.random() * 70) + 1
-		numberSet.add(randomNumber)
+	if (obsEnabled === 'true') {
+		if (sceneChangeLock.active) {
+			client.say(
+				channel,
+				'Hold on! A scene change is already in progress. Please wait.'
+			)
+			return
+		}
+
+		sceneChangeLock.active = true // lock all scene changes
+
+		const numberSet = new Set()
+		while (numberSet.size < 6) {
+			const randomNumber = Math.floor(Math.random() * 70) + 1
+			numberSet.add(randomNumber)
+		}
+		const lotteryNumbers = Array.from(numberSet)
+			.sort((a, b) => a - b)
+			.join(', ')
+
+		const message = `${tags.username}'s lucky numbers: ${lotteryNumbers}`
+
+		try {
+			const currentScene = await obs.call('GetCurrentProgramScene')
+			const currentSceneName = currentScene.currentProgramSceneName
+
+			// Send a WebSocket message to the proxy server
+			const ws = new WebSocket(wsAddress)
+
+			ws.on('open', () => {
+				console.log('WebSocket connection opened')
+				ws.send('!lotto', message) // Send the trigger message
+				ws.close()
+			})
+
+			ws.on('error', (error) => {
+				console.error('WebSocket error:', error.message)
+			})
+
+			await obs.call('SetCurrentProgramScene', {
+				sceneName: 'PI CAM',
+			})
+
+			console.log(`Switched to PiCam scene: PI CAM`)
+
+			setTimeout(async () => {
+				await obs.call('SetCurrentProgramScene', {
+					sceneName: currentSceneName,
+				})
+				console.log(`Reverted to previous scene: ${currentSceneName}`)
+				sceneChangeLock.active = false // unlock after the scene reverts
+			}, 12000)
+			client.say(
+				channel,
+				`Let's check the PiBot for ${tags.username}'s lucky numbers!`
+			)
+		} catch (error) {
+			console.error('Error handling Lotto command:', error.message)
+			client.say(
+				channel,
+				"Sorry, I'm having trouble with the lottery right now!"
+			)
+			sceneChangeLock.active = false // unlock if an error occurs
+		}
+	} else {
+		client.say(channel, 'No lottery to show you right now!')
 	}
-	const lotteryNumbers = Array.from(numberSet)
-		.sort((a, b) => a - b)
-		.join(', ')
-
-	client.say(channel, `${tags.username}'s lucky numbers are: ${lotteryNumbers}`)
-
-	// if (obsEnabled === 'true') {
-	// 	client.say(
-	// 		channel,
-	// 		`${tags.username}, check the PiBot for your lucky numbers!`
-	// 	)
-
-	// 	let currentScene
-	// 	let message = `${tags.username}'s lucky numbers: ${lotteryNumbers}`
-
-	// 	await obs.call('GetSceneList').then((data) => {
-	// 		console.log('CURRENT SCENE: ', data.currentProgramSceneName)
-	// 		currentScene = data.currentProgramSceneName
-	// 	})
-
-	// 	axios
-	// 		.post('http://192.168.86.50:5000/display_lotto', {
-	// 			message: message,
-	// 		})
-	// 		.then((response) => {
-	// 			console.log('Response from Pi: ', response.data)
-	// 		})
-	// 		.catch((error) => {
-	// 			console.log('ERROR: ', error)
-	// 		})
-	// 	setTimeout(async () => {
-	// 		await obs
-	// 			.call('SetCurrentProgramScene', { sceneName: 'PI CAM' })
-	// 			.then((data) => console.log(data))
-	// 	}, 2000)
-
-	// 	setTimeout(() => {
-	// 		obs.call('SetCurrentProgramScene', { sceneName: `${currentScene}` })
-	// 	}, 12000)
-	// } else {
-	// 	client.say(
-	// 		channel,
-	// 		`${tags.username}'s lucky numbers are: ${lotteryNumbers}`
-	// 	)
-	// }
 }
 
 module.exports = {
-	lottoCommand: lottoCommand,
+	lottoCommand,
 }
 
-// local URL for e-Paper display server
-// http://192.168.86.50:8000/display
+
