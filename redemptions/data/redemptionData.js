ad7a9dd8269135ae4d3773d09e3837f817df157c
marcusmcb <marcusmcb@gmail.com>
2025-06-28 21:59:13 -0700

    scene changes triggerring correctly on channel redemption

--- redemptions/data/redemptionData.js
+++ redemptions/data/redemptionData.js
@@ -1,18 +1,27 @@
 const redemptionData = {
   take_us_down_pch: {
     scene_name: 'HUNTINGTON TL',
-    text: 'Taking us down the Pacific Coast Highway! 🌊',
+    text: 'Time for a quick cruise down Pacific Coast Highway 🌊',
     error_text: "Looks like the 'Take Us Down PCH' redemption isn't working right now.",
-    display_time: 10000,
+    display_time: 14000,
   }, 
   go_play_with_squirrels: {
-
+    scene_name: 'GO PLAY WITH DASH 2',
+    text: "Let's go say hello to Dash! 🐿️",
+    error_text: "Looks like the 'Go Play with Squirrels' redemption isn't working right now.",
+    display_time: 10000,
   }, 
   sunset_time: {
-
+    scene_name: 'SUNSET TL',
+    text: "Dig those clouds rollin' in from the Pacific 🌅",
+    error_text: "Looks like the 'Sunset Time' redemption isn't working right now.",
+    display_time: 15000,
   }, 
   water_those_dogs: {
-
+    scene_name: "WATER DOGS",
+    text: "Time to water those dogs! 🐶💦",
+    error_text: "Looks like the 'Water Those Dogs' redemption isn't working right now.",
+    display_time: 11000,
   },
 }
 
--- redemptions/redemptionHandler.js
+++ redemptions/redemptionHandler.js
@@ -4,42 +4,55 @@ const redemptionHandler = async (
 	obs,
 	client,
 	sceneChangeLock,
-  channel,
+	channel,
 	redemptionTitle
 ) => {
 	sceneChangeLock.active = true
-  let redemptionSceneName = ""
 
-  switch (redemptionTitle) {
-    case "Take Us Down PCH":
-      console.log("Redemption: Take Us Down PCH")
-      redemptionSceneName = "HUNTINGTON TL"
-      break
-    case "Go Play with Squirrels":
-      console.log("Redemption: Go Play with Squirrels")
-      break
-    default:
-      console.log("Unknown redemption title:", redemptionTitle)
-      client.say(channel, "Unknown redemption title.")
-      sceneChangeLock.active = false
-      return
-  }
+	let redemption  
 
+	switch (redemptionTitle) {
+		case 'Take Us Down PCH':
+			console.log('Redemption: Take Us Down PCH')
+			redemption = redemptionData.take_us_down_pch
+			break
+		case 'Go Play With Squirrels':
+			console.log('Redemption: Go Play with Squirrels')
+			redemption = redemptionData.go_play_with_squirrels
+			break
+		case 'Sunset Time':
+			console.log('Redemption: Sunset Time')
+			redemption = redemptionData.sunset_time
+			break
+		case 'Water Those Dogs':
+			console.log('Redemption: Water Those Dogs')
+			redemption = redemptionData.water_those_dogs
+			break
+		default:
+			console.log('Unknown redemption title:', redemptionTitle)
+			client.say(channel, 'Unknown redemption title.')
+			sceneChangeLock.active = false
+			return
+	}
 
-
-
-
-  
 	try {
-    const currentScene = await obs.call('GetCurrentProgramScene')
-    const currentSceneName = currentScene.currentProgramSceneName
-
-  } catch (error) {
-    console.error('Error getting current scene:', error.message)
-    client.say(channel, "Couldn't retrieve the current scene.")
-    sceneChangeLock.active = false
-    return
-  }
+		const currentScene = await obs.call('GetCurrentProgramScene')
+		const currentSceneName = currentScene.currentProgramSceneName
+		await obs.call('SetCurrentProgramScene', {
+			sceneName: redemption.scene_name,
+		})
+		client.say(channel, redemption.text)
+		setTimeout(async () => {
+			await obs.call('SetCurrentProgramScene', { sceneName: currentSceneName })
+			console.log(`Reverted to previous scene: ${currentSceneName}`)
+			sceneChangeLock.active = false
+		}, redemption.display_time)
+	} catch (error) {
+		console.error('Error getting current scene:', error.message)
+		client.say(channel, "I couldn't find the scene for that redemption.")
+		sceneChangeLock.active = false
+		return
+	}
 }
 
 module.exports = { redemptionHandler }
